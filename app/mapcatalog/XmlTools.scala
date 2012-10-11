a776e67e51af4dfab31d3aa917902b3f839073f1
nicolas-f <github@nettrader.fr>
2012-10-11 11:41:45 +0200

    POST MapContext done, serialisation of Workspaces and MapContext done

---
+++ app/mapcatalog/XmlTools.scala
@@ -0,0 +1,12 @@
+package mapcatalog
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fortin
+ * Date: 11/10/12
+ * Time: 08:47
+ * To change this template use File | Settings | File Templates.
+ */
+object XmlTools {
+
+}
