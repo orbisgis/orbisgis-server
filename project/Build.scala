ea7d51db80046427429cd81b074f72ee74e67b8f
Alexis Guéganno <alexis.gueganno@ec-nantes.fr>
2013-07-05 15:08:55 +0200

    increases the version in order not to face caching issues...

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "org.orbisgis.server" % "orbiswms-lib" % "1.0-SNAPSHOT" changing(),
+      "org.orbisgis.server" % "orbiswms-lib" % "1.0.1-SNAPSHOT" changing(),
       "com.sun.xml.bind" % "jaxb-impl" % "2.2.2",
       "com.kitfox.svg" % "svg-salamander" % "1.0" from "http://repo.orbisgis.org/com/kitfox/svg/svg-salamander/1.0/svg-salamander-1.0.jar",
       "javax.media" % "jai_core" % "1.1.3" from "http://repo.orbisgis.org/javax/media/jai_core/1.1.3/jai_core-1.1.3.jar",
