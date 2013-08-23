6f51d8f55828ea855bfdd7054475e5f9411c147d
Mario <mariojothy@gmail.com>
2013-08-23 15:29:17 +0200

    New module to enable rich text descriptions
    Signed-off-by: Mario <mariojothy@gmail.com>

---
+++ play/public/javascripts/test.js
@@ -0,0 +1,20 @@
+tinymce.init({
+    selector: "textarea",
+    theme: "modern",
+    plugins: [
+         "advlist autolink link image lists charmap preview hr anchor pagebreak spellchecker",
+         "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime nonbreaking",
+         "save table contextmenu directionality paste textcolor"
+   ],
+   content_css: "css/content.css",
+   toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | preview fullpage | forecolor backcolor",
+   style_formats: [
+        {title: 'Bold text', inline: 'b'},
+        {title: 'Red text', inline: 'span', styles: {color: '#ff0000'}},
+        {title: 'Red header', block: 'h1', styles: {color: '#ff0000'}},
+        {title: 'Example 1', inline: 'span', classes: 'example1'},
+        {title: 'Example 2', inline: 'span', classes: 'example2'},
+        {title: 'Table styles'},
+        {title: 'Table row 1', selector: 'tr', classes: 'tablerow1'}
+    ]
+ }); 
